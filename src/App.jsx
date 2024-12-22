9c530ef7d5314860296b2afa8f1c305187e8fcb1
VANSH GOYAL <140587946+Vansh3005@users.noreply.github.com>
2024-12-22 12:08:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+   
+    <h1 className="text-3xl font-bold underline text-red-900">
+      Hello world!
+    </h1>
+ 
+    </div>
+  )
+}
